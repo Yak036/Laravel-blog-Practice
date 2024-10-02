699a98a712011cfc87bffefa95d4725640cbff14
ramces <ramcesvedes@gmail.com>
2024-10-02 17:07:00 -0400

    panel

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 use App\Http\Controllers\PageController;
-
-
+use App\Http\Controllers\PostController;
 
 // ! Mala practica
 
@@ -30,5 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('posts', PostController::class)->except('show');
 
 require __DIR__.'/auth.php';
